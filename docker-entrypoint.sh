f4415e66689de98ac267a02416811a262b56bacd
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-03-23 06:48:24 -0400

    Optimize docker-entrypoint for virtualenv
    Enabled virtual environment in the entrypoint script to ensure isolation of Python dependencies. The update includes sourcing the activate script after setting up the models. Also, environmental variable setting and the make run command were divided into separate lines for clarity. These changes ensure the application runs within the intended Python environment, reducing the risk of dependency conflicts.
    
    Refs: #SC1091, #SC3046

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,10 @@
 set -e
 # run setup to download models
 /home/nonroot/app/.venv/bin/python scripts/setup
+# shellcheck disable=SC1091
+# shellcheck disable=SC3046
+source /home/nonroot/app/.venv/bin/activate
 # run app
-export PGPT_PROFILES=ollama make run
+export PGPT_PROFILES=ollama 
+make run
 #/home/nonroot/app/.venv/bin/python -m private_gpt
